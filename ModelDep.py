a191f350868172caa42e1549aead23ae8cd93a0a
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-10-03 23:46:11 +0000

    First commit

---
+++ ModelDep.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+import json
+import numpy as np
+import sys
+import os
+import joblib
+import xgboost as xgb
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+import random
+import time
+
+
+class Model():
+    def __init__(self):
+        self.wd = os.path.dirname(os.path.abspath(__file__))
+        file_list ={'XGBoost Model':'XGBoost.pkl' , 'City Label Encoder':'LECity.pkl','One Hot Encoder': 'oheCT04.pkl'}
+        dpnds_path = 'dpnds'
+        dpnds_path = os.path.join(self.wd,'dpnds')
+        fileMissing = False
+        if os.path.isdir(dpnds_path):
+            self.files = [f for f in os.listdir(dpnds_path) if os.path.isfile(os.path.join(dpnds_path, f))]
+        else:
+            print ('Dependencies Folder Not Found')
+            sys.exit(-1)
+
+        for i in file_list.keys():
+            if file_list[i] in self.files:
+                print (i, ': Found')
+            else:
+                print (i,': Not Found')
+                fileMissing =True
+        if fileMissing:
+            print ('Check Missing Dependencies')
+            sys.exit(-1)
+
+    def get_dpnds(self):
+        file_list = self.files
+        dpnds_path = os.path.join(self.wd,'dpnds')
+        for i in  file_list:
+            if i == 'XGBoost.pkl':
+                modelFile = os.path.join(dpnds_path,i)
+            elif i == 'LECity.pkl':
+                cityLEFile = os.path.join(dpnds_path,i)
+            elif i == 'oheCT04.pkl':
+                oheCTFile = os.path.join(dpnds_path,i)
+
+        try:
+            self.model = joblib.load(modelFile)
+        except Exception as e:
+            print ('Error Downloading Model: ', e)
+            sys.exit(-1)
+        try:
+            self.cityLE = joblib.load(cityLEFile)
+        except Exception as e:
+            print ('Error Downloading City Label Encoder: ', e)
+            sys.exit(-1)
+        try:
+            self.oheCT = joblib.load(oheCTFile)
+        except Exception as e:
+            print ('Error Downloading One Hot Encoder: ', e)
+            sys.exit(-1)
+
+
+    def data_encoding(self, df):
+        df['city'] = self.cityLE.transform(df['city'])
+        #print(df.head(1))
+        X = df.iloc[:,:]
+        #print (X[:,:])
+        X = self.oheCT.transform(X)
+        return X
+
+    def predict (self, X):
+        price = self.model.predict(X)
+        price = round(price[0], 2)
+        price = np.expm1(price)
+        return price
+
+
---
+++ WebApp.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import json
+import os
+import ModelDep
+import datetime
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+#os.system('streamlit run webApp.py')
+
+class webApp():
+    def __init__(self):
+        self.Price = 0.0
+        with open('Columns.json', 'r') as file:
+            columns = json.load(file)
+        self.cityList = columns["City"]
+        self.levelsList = columns["Levels"]
+        self.homeTypeList = columns["HomeType"]
+
+        st.title('California Housing Price Predector')
+        self.pageColumns = st.columns((1,1))
+
+        with self.pageColumns[0]:
+             self.homeType = st.selectbox(label= 'Select Home Type', options=self.homeTypeList, key=1)
+             self.City = st.selectbox('Select City', options = self.cityList, key=2)
+             self.level = st.selectbox('Select Number of Stories', options=self.levelsList, key=3)
+             self.yearBuilt = st.select_slider('Select Year Built', options=range(1850,2025), key=4)
+             self.livingArea = st.slider('Select Level Area',min_value=100, max_value=6000, step=10, key=5)
+             self.bedRooms = st.slider('How Many Bedrooms', min_value=1, max_value=15, key=6)
+             self.bathRooms = st.slider('How Many Bathrooms', min_value=1, max_value=15, key=7)
+
+        with self.pageColumns[1]:
+             self.hasParking = st.radio('Parking', options={'Yes': True, 'No':False}, horizontal=True, key=8)
+             self.hasGarage = st.radio('Garage', options={'Yes': True, 'No':False}, horizontal=True, key=9)
+             self.hasPool = st.radio('Pool', options={'Yes': True, 'No':False}, horizontal=True, key=10)
+             self.hasSpa = st.radio('Spa', options={'Yes': True, 'No':False}, horizontal=True, key=11)
+             self.datePosting = st.date_input('When to Buy', value=None, key=12)
+             self.button = st.button('Predict',on_click=self.predict , key=13)
+
+    
+    def predict(self):
+        predictedPrice = st.empty()
+        homeType = self.homeType
+        city = self.City
+        level = self.level
+        yearBuilt = self.yearBuilt
+        livingArea = self.livingArea
+        bedRooms = self.bedRooms
+        bathRooms = self.bathRooms
+        hasParking = self.hasParking
+        hasGarage = self.hasGarage
+        hasPool = self.hasPool
+        hasSpa = self.hasSpa
+        datePosting = self.datePosting
+
+        if homeType == 'Single Family':
+            home = 'SINGLE_FAMILY'
+        elif homeType == 'Condo':
+            home = 'CONDO'
+        elif homeType == 'Townhouse':
+            home = 'TOWNHOUSE'
+
+        parking = 1 if hasParking == 'Yes' else 0
+        garage = 1 if hasGarage == 'Yes' else 0
+        pool = 1 if hasPool == 'Yes' else 0
+        spa = 1 if hasSpa == 'Yes' else 0
+
+        age = 2022-yearBuilt
+
+        month = datePosting.month
+        if month in [6,7,8]:
+            season = 'summer'
+        elif month in [9,10,11]:
+            season = 'fall'
+        elif month in [12,1,2]:
+            season = 'winter'
+        elif month in [3,4,5]:
+            season = 'spring'
+        
+        df = pd.DataFrame([[level,home, season, city, yearBuilt, livingArea, bathRooms, bedRooms, parking, garage, pool, spa, age ]],columns=['levels', 'homeType', 'postingSeason', 'city', 'yearBuilt','livingAreaValue', 'bathrooms', 'bedrooms', 'parking', 'hasGarage','pool', 'spa', 'Age'])
+
+        model = ModelDep.Model()
+        model.get_dpnds()
+        X = model.data_encoding(df)
+        Price = model.predict(X)
+        self.Price = Price
+        self.printPrice()
+    
+    def printPrice(self):
+        with self.pageColumns[1]:
+            st.text(f'Predicted Price: {self.Price} USD')
+        
+
+web = webApp()
+
+

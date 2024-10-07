a866b76b4f8b2aee90e0eb5623ff333203a5cd7d
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-10-07 00:58:08 +0000

    Modify Map

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,42 +10,44 @@ import folium
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim(user_agent="myGeocoder")
+with st.container():
+    geolocator = Nominatim(user_agent="myGeocoder")
 
-st.title("Map to Select Location")
+    st.title("Map to Select Location")
 
-latitude, longitude = 37.7749, -122.4194  # San Francisco
-# Create a map centered around the starting point
-m = folium.Map(location=[latitude, longitude], zoom_start=13)
-clicked_location = st_folium(m, width=700, height=500)
-# Check if a location was clicked
-if clicked_location and clicked_location['last_clicked']:
-    lat = clicked_location['last_clicked']['lat']
-    lon = clicked_location['last_clicked']['lng']
-     # Perform reverse geocoding to get address information
-    try:
-        location = geolocator.reverse((lat, lon), exactly_one=True, language="en")
-    except:
-        pass
+    latitude, longitude = 37.7749, -122.4194  # San Francisco
+    # Create a map centered around the starting point
+    m = folium.Map(location=[latitude, longitude], zoom_start=13)
+    clicked_location = st_folium(m, width=700, height=500)
+    # Check if a location was clicked
+    if clicked_location and clicked_location['last_clicked']:
+        lat = clicked_location['last_clicked']['lat']
+        lon = clicked_location['last_clicked']['lng']
+        # Perform reverse geocoding to get address information
+        location = None
+        try:
+            location = geolocator.reverse((lat, lon), exactly_one=True, language="en")
+        except:
+            pass
 
-    if location:
-        address = location.address
-        st.write(f"**Address:** {address}")
+        if location:
+            address = location.address
+            st.write(f"**Address:** {address}")
 
-        # Extract details from the address
-        address_details = location.raw['address']
-        
-        city = address_details.get('city', '')
-        county = address_details.get('county', '')
-        street = address_details.get('road', '')
-        zip_code = address_details.get('postcode', '')
+            # Extract details from the address
+            address_details = location.raw['address']
+            
+            city = address_details.get('city', '')
+            county = address_details.get('county', '')
+            street = address_details.get('road', '')
+            zip_code = address_details.get('postcode', '')
 
-        st.write(f"**City:** {city}")
-        st.write(f"**County:** {county}")
-        st.write(f"**Street:** {street}")
-        st.write(f"**Zip Code:** {zip_code}")
-    else:
-        st.write("No address found for this location.")
+            st.write(f"**City:** {city}")
+            st.write(f"**County:** {county}")
+            st.write(f"**Street:** {street}")
+            st.write(f"**Zip Code:** {zip_code}")
+        else:
+            st.write("No address found for this location.")
 
 
 with open('Columns.json', 'r') as file:
@@ -79,39 +81,3 @@ with st.sidebar:
     with st.container():  # container3
         datePosting = st.date_input('When to Buy', value=datetime.datetime.now(), key=12)
         button = st.button('Predict', key=13)
-
-
-
-
-
-
-
-
-
-'''col1, col2, col3, col4 =st.columns(4)
-sidebar = st.sidebar
-container1 = st.container(border= True)
-container2 = st.container(border= True)
-container3 = st.container(border= True)
-
-with sidebar:
-    with container1:
-        homeType = st.selectbox(label= 'Select Home Type', options=homeTypeList, key=1)
-        City = st.selectbox('Select City', options = cityList, key=2)
-        level = st.selectbox('Select Number of Stories', options=levelsList, key=3)
-        yearBuilt = st.select_slider('Select Year Built', options=range(1850,(datetime.datetime.now().year +1)), key=4)
-        livingArea = st.slider('Select Level Area',min_value=100, max_value=6000, step=10, key=5)
-        bedRooms = st.slider('How Many Bedrooms', min_value=1, max_value=15, key=6)
-        bathRooms = st.slider('How Many Bathrooms', min_value=1, max_value=15, key=7)
-    with container2:
-        with col1:
-            hasParking = st.radio('Parking', options={'Yes': True, 'No':False}, horizontal=True, key=8)
-        with col2:
-            hasGarage = st.radio('Garage', options={'Yes': True, 'No':False}, horizontal=True, key=9)
-        with col3:
-            hasPool = st.radio('Pool', options={'Yes': True, 'No':False}, horizontal=True, key=10)
-        with col4:
-            hasSpa = st.radio('Spa', options={'Yes': True, 'No':False}, horizontal=True, key=11)
-    with container3:
-        datePosting = st.date_input('When to Buy', value=None, key=12)
-        button = st.button('Predict', key=13)'''

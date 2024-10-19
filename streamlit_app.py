731bc7e5ff90f23ef3b875275304d9991ee6d955
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-10-19 11:43:27 +0000

    5

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,7 +178,7 @@ with tab2:
         st.markdown(':flag-ae:[Shaheer Airaj Ahmed](https://www.linkedin.com/in/shaheerairaj/)', unsafe_allow_html= True)
         st.divider()
         st.subheader('Project members:')
-        st.markdown(':flag-jo:[Mohammad M Zakarneh](https://www.linkedin.com/in/mohamed-zakarneh/)', unsafe_allow_html= True)
+        st.markdown('🇯🇴 [Mohammad M Zakarneh](https://www.linkedin.com/in/mohamed-zakarneh/)', unsafe_allow_html= True)
         st.markdown(':flag-🇺🇿:[Davron Abdukhakimov](https://www.linkedin.com/in/davron-abdukhakimov-90aab4264/)', unsafe_allow_html= True)
         #st.markdown('🇦🇺 Soumya Tamhankar')
         #st.markdown('🇺🇸 [Amos Anzele](https://www.linkedin.com/in/aanzele/)')
